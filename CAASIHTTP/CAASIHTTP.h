f09125377d198bec398acfb0265fb3ca46da958a
魏凯凯 <137067401@qq.com>
2014-10-16 09:28:34 +0800

    Initial Commit

---
+++ CAASIHTTP/CAASIHTTP.h
@@ -0,0 +1,13 @@
+//
+//  CAASIHTTP.h
+//  CAASIHTTP
+//
+//  Created by Caland on 14-10-16.
+//  Copyright (c) 2014年 Caland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CAASIHTTP : NSObject
+
+@end
---
+++ CAASIHTTP/CAASIHTTP.m
@@ -0,0 +1,13 @@
+//
+//  CAASIHTTP.m
+//  CAASIHTTP
+//
+//  Created by Caland on 14-10-16.
+//  Copyright (c) 2014年 Caland. All rights reserved.
+//
+
+#import "CAASIHTTP.h"
+
+@implementation CAASIHTTP
+
+@end
